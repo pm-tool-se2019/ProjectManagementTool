7c5230dd70e432d0f6aa8d61f24600e92ed9593e
ATEZ <shieldnet64@gmail.com>
2019-05-21 19:15:10 +0900

    Changed Structure

--- src/main/java/Calendar/MyCalendar.java
+++ src/main/java/Calendar/MyCalendar.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-/*
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-*/
+
 import DataClass.Task;
 
 class CalendarDataManager{
@@ -19,6 +19,7 @@ class MyCalendar {
     static final int CAL_WIDTH = 7;
     static final int CAL_HEIGHT = 6;
 
+    //Information of calendar
     private Date[][] calendar_dates = new Date[CAL_HEIGHT][CAL_WIDTH];
     private Calendar calendar;
     private ArrayList<Task> task_list;
@@ -27,14 +28,16 @@ class MyCalendar {
     private int now_year;
     private int now_month;
     private int now_day;
-    // Pointing(
+
+    // Pointing
+
 
     //Constructor Of Calendar class
     //Take task_list and make calendar
     MyCalendar(ArrayList<Task> task_list) {
         this.task_list = task_list;
         this.calendar = Calendar.getInstance();
-        this.now_year = calendar.set(Calendar.);
+        this.now_year = 0;
     }
 
     //Default Constructor.
@@ -43,9 +46,4 @@ class MyCalendar {
         this.task_list = new ArrayList<Task>();
         this.calendar = Calendar.getInstance();
     }
-}
-
-
-
-
 }
