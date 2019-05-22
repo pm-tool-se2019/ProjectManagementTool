cb7fd3029602c231952c1dd3bd08f8aa27e0ab40
kze <esimileiar81@gmail.com>
2019-05-22 17:36:02 +0900

    UI_Reconstruct
    Reconstruct Hierarchy

---
+++ src/main/java/UI/Main.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    //private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("UI");
+
+        //initRootLayout();
+
+        showMain();
+    }
+
+
+    public void showMain() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../main/java/UI/Main.fxml"));
+            AnchorPane main = (AnchorPane) loader.load();
+            
+            Scene scene = new Scene(main);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
