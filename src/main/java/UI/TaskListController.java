cf659c7fe083702e60c8afca31c242b9a74ecda3
sixFires <48874365+sixFires@users.noreply.github.com>
2019-06-09 14:58:24 +0900

    Add files via upload
    finished TaskList.fxml.
    Not finished Controller.java

--- src/main/java/UI/TaskListController.java
+++ src/main/java/UI/TaskListController.java
@@ -2,17 +2,23 @@ package main.java.UI;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import main.java.TaskLists.TaskListFuncs;
 
+import java.awt.*;
 import java.util.ResourceBundle;
 
 public class TaskListController{
     @FXML
-
+    private AnchorPane baseAnchorPane;
+    @FXML
+    private Button back;
+    
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TaskList.fxml"));
 
 
 
