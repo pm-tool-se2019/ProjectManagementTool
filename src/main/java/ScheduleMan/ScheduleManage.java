be104bca9fa29cc306975c94fb9e07dbc542fa63
shieldnet64@gmail.com <shieldnet64@gmail.com>
2019-06-09 14:27:41 +0900

    Build Error Solved

--- src/main/java/ScheduleMan/ScheduleManage.java
+++ src/main/java/ScheduleMan/ScheduleManage.java
@@ -1,8 +1,7 @@
-﻿package main.java.ScheduleMan;
+package main.java.ScheduleMan;
 
 import main.java.DataClass.Task;
 import java.io.UnsupportedEncodingException;
-
 
 import main.java.ScheduleMan.firebase4j.firebasesrc.model.FirebaseResponse;
 import main.java.ScheduleMan.firebase4j.firebasesrc.service.Firebase;
@@ -47,7 +46,7 @@ public class ScheduleManage{
         * But, the object will not be deleted by GC? */
         Task target = searchFromTaskList(id);
         if(!current_user_task_list.contains(target)){
-            System.out.println("해당 Task가 존재하지 않습니다.");
+            //System.out.println("�ش� Task�� �������� �ʽ��ϴ�.");
             return;
         }
         else{
@@ -81,52 +80,4 @@ public class ScheduleManage{
         return receivedtask;
     }
 
-
-    // For testing
-
-//    public static void main(String[] args) throws FirebaseException, UnsupportedEncodingException {
-//        Firebase firebase = new Firebase(firebase_baseUrl);
-//        ArrayList<Task> tasks = new ArrayList<>();
-//        Task task= new Task(11, "TL1", "making TaskList1 function", 2019, 01, 01, 2019, 05, 29, "DOING", 1);
-//        Task task2 = new Task(12, "TL2", "making TaskList2 function", 2019, 01, 01, 2019, 05, 29, "TODO", 2);
-//        Task task3 = new Task(13, "TL3", "making TaskList3 function", 2019, 01, 01, 2019, 05, 28, "TODO", 1);
-//        Task task4 = new Task(14, "TL4", "making TaskList4 function", 2019, 01, 01, 2019, 05, 27, "TODO", 3);
-//
-//        tasks.add(task);
-//        tasks.add(task2);
-//        tasks.add(task3);
-//        tasks.add(task4);
-
-        // DATA INSERT TEST
-
-//        for (Task instance : tasks){
-//            Gson gson = new Gson();
-//            String jsonified_task = gson.toJson(instance);
-//            firebase.put("/Task/" + instance.getId(),jsonified_task);
-//
-//        }
-
-        // DATA PULL TEST
-        // Data not found check -> equals("null").
-        // Because RawBody returns string "null" when failed to found data.
-
-//        int pull_id = 19;
-//        FirebaseResponse response = firebase.get("/Task/"+(pull_id));
-//        System.out.println(response);
-//        Gson gson = new Gson();
-//        String taskjson = response.getRawBody();
-//        System.out.println(taskjson);
-//        if(taskjson.equals("null")){
-//            System.out.println("Null");
-//            return;
-//        }
-//        Task receivedtask = gson.fromJson(taskjson, Task.class);
-//        System.out.println(receivedtask.getDescription()+ '\n' + receivedtask.getId() + '\n' + receivedtask.getState());
-
-//    }
-
-
 }
-
-
-
